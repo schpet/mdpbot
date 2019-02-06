6f5fede23a64ab6fe3b171693e344632fcbb6580
Peter Schilling <peter@peterschilling.org>
2019-02-05 20:32:11 -0800

    Grab a list of venue names

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -5,29 +5,52 @@ const chromium = require("chrome-aws-lambda")
 const puppeteer = chromium.puppeteer
 
 // https://github.com/alixaxel/chrome-aws-lambda/issues/7#issuecomment-450402988
+
 exports.handler = async function http(req) {
   console.log(req)
+
+  const username = process.env.USERNAME
+  const password = process.env.PASSWORD
 
   const browser = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath,
-    headless: chromium.headless
+    headless: chromium.headless,
+    // slowMo: 200
   })
 
   // const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  // await page.goto("https://mydiscoverypass.quipugroup.net")
-  await page.goto("http://peterschilling.org")
+
+
+  await page.goto("https://mydiscoverypass.quipugroup.net")
+  await page.type("#patronNumber", username)
+  await page.type("#patronPassword", password)
+  await page.click("#vpass_loginButton")
+
+  const venueSelector = ".fieldsetVenueOffers h2"
+
+  await page.goto("https://mydiscoverypass.quipugroup.net/?method=Venues")
+
+  await page.waitForSelector(venueSelector);
+
+  // Extract the results from the page.
+  const venueNames = await page.evaluate(venueSelector => {
+    const h2s = Array.from(document.querySelectorAll(venueSelector));
+    return h2s.map(node => {
+      return node.textContent
+    });
+  }, venueSelector);
+
   // await page.screenshot({ path: "example.png" })
   const title = await page.title()
-  console.log(`title: ${title}`)
 
   await browser.close()
 
   return {
     type: "text/html; charset=utf8",
-    body: `<h1>Hello world! title=${title}</h1>`
+    body: `<pre>${JSON.stringify({ title, venueNames }, null, '  ')}</pre>`
   }
 }
 
